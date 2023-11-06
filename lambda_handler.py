5ca7ff9ae52b70b285c1b9baf622af39d5f15345
Bat Lamjav <39855553+l3atjin@users.noreply.github.com>
2023-11-05 19:57:50 -0800

    Now writes back to the bucket

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,5 +1,28 @@
 import boto3
 from urllib.parse import unquote_plus
+import os
+import json
+
+def upload_file(label, key, bucket):
+    """Upload a file to an S3 bucket
+
+    :param key: Name of image file
+    :param bucket: Bucket to upload to
+    :param label: Content of the new file, a json
+    :return: True if file was uploaded, else False
+    """
+    file_name = key.split(".")[0] + ".json"
+    
+    print("filename is", file_name)
+    print("bucket is", bucket)
+
+    object_name = os.path.basename(file_name)
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    response = s3_client.put_object(Bucket=bucket, Key=file_name, Body=json.dumps(label))
+    
+    return True
 
 
 def label_function(bucket, name):
@@ -31,4 +54,8 @@ def lambda_handler(event, context):
         print(f"This is my key {key}")
 
     my_labels = label_function(bucket=bucket, name=key)
+    
+    print("This is a picture of", my_labels[0]["Name"])
+    upload_file(my_labels, key, bucket)
+    
     return my_labels
